f65ab2a421db664caa8a7c959b3e4ffcc0798919
Diego Vargas <yeyox1945@gmail.com>
2024-04-04 14:00:39 -0600

    Removed shell route experiment

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,22 +1,8 @@
 import 'package:go_router/go_router.dart';
-import 'package:social_app/presentation/screens/views/views.dart';
 import '../../presentation/screens/screens.dart';
 
 final appRouter = GoRouter(
   routes: [
-    // ShellRoute(
-    //   routes: [
-    //     GoRoute(
-    //       path: '/profile',
-    //       builder: (context, state) => const ProfileView(),
-    //     ),
-    //     GoRoute(
-    //       path: '/posts',
-    //       builder: (context, state) => const PostsView(),
-    //     ),
-    //   ],
-    //   builder: (context, state, child) => HomeScreen(child: child),
-    // ),
     GoRoute(
       path: '/',
       builder: (context, state) => const HomeScreen(),
